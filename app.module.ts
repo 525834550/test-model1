a0f43e7a0fb13cc367203cd4519bf981cead59bf
525834550 <32120516+525834550@users.noreply.github.com>
2017-09-20 11:45:57 +0530

    testing

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {PopupModule} from 'ng2-opd-popup';
+
+ 
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,	 
+    PopupModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
